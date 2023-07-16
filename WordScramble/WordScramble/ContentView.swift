1b84aacdd1f9479dfb0b4a02833e09cc31a540b8
ZhuravlevMax <l9sik3@gmail.com>
2023-07-16 12:53:13 +0300

    main items and func addNewWord() added

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userWords = [String]()
+    @State private var newWord = ""
+    @State private var rootWord = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }// Section
+                
+                Section {
+                    ForEach(userWords, id: \.self) { word in
+                        
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    } //ForEach
+                }//Section
+                
+            }// List
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }//NavigationView
+    }//body
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            userWords.insert(answer, at: 0)
         }
-        .padding()
-    }
-}
+        newWord = ""
+    }//addNewWord()
+}// struct ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
